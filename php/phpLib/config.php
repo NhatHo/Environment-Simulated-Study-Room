db8f82768a5c1dad7a554670c236b1642c7dd186
nhat ho <paullightman@gmail.com>
2014-12-02 17:32:07 -0500

    config File

--- php/phpLib/config.php
+++ php/phpLib/config.php
@@ -12,4 +12,12 @@
 	define ("AUDIO", "mp3");
 	define ("VIDEO", "mov mp4 f4v flv avi");
 	define ("SECURE", FALSE);
+	define ("ABSOLUTEPATH", "C://xampp/htdocs/Environment-Simulated-Study-Room/data");
+	define ("SDPLOCATION", "localhost/Environment-Simulated-Study-Room/data");
+	define ("AUDIOPI", "10.0.0.21");
+	define ("PROJECTOR1", "10.0.0.22");
+	define ("PROJECTOR2", "10.0.0.23");
+	define ("PROJECTOR3", "10.0.0.24");
+	define ("IRRECEIVER", "10.0.0.25");
+	define ("PORT", 3010);
 ?>
